1c941d5c92e1fd592bcb5964c10af568283b781d
Nik Payne <nikolasapayne@gmail.com>
2018-03-31 15:47:36 -0500

    updated the media.py file with docstring attribute comments

--- media.py
+++ media.py
@@ -3,8 +3,22 @@
 
 
 class Movie():
-    # This initializes the Movie object.
-    # It takes movie information in as arguments.
+    """
+    The Movie class creates Movies objects.
+    It takes in key information about a movies, as a series of arguments.
+
+    Args:
+        movie_title (str): The name of the movie.
+        movie_storyline (str): A short description of the movie's storyline.
+        movie_poster_image (str): URL link to the movie's cover image.
+        movie_trailer (str): Youtube URL link to the movie trailer.
+
+    Attributes:
+        title (str): The name of the moview.
+        storyline (str): A short description of the storyline.
+        poster_image_url (str): URL link to the movie's cover image.
+        trailer_youtube_url (str):  Youtube shortlink tp the movie trailer.
+    """
     def __init__(
             self,
             movie_title,
